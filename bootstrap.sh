66d472b96ea71404fbdcdcf88d74fc0f13807084
Gil Freund <gilf@sysnet.co.il>
2026-04-29 21:57:58 +0300

    Set ANSIBLE_COLLECTIONS_PATH to suppress galaxy warning

--- bootstrap.sh
+++ bootstrap.sh
@@ -167,7 +167,7 @@ install_ansible_requirements() {
     return
   fi
   log "Installing Ansible Galaxy requirements..."
-  ansible-galaxy collection install -r "${REPO_DIR}/requirements.yml" -p "${collections_dir}" --force
+  ANSIBLE_COLLECTIONS_PATH="${collections_dir}" ansible-galaxy collection install -r "${REPO_DIR}/requirements.yml" -p "${collections_dir}" --force
 }
 
 run_playbook() {
